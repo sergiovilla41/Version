c70838e0b2c13fe3b6fac441aedf847614571319
Sergio <serfusion41@gmail.com>
2024-01-16 22:40:20 -0500

    Mensaje del commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PadreComponent } from './Component/padre/padre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PadreComponent
   ],
   imports: [
     BrowserModule,
